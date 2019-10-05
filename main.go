95c19bb7739c28863d1873ff54e70572e2f4f509
Ashutosh Dubey <idkashutosh@gmail.com>
2019-10-05 13:57:41 +0530

    removed mongodb

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"webkodes.com/server/routes"
 	"webkodes.com/server/utils"
 )
@@ -17,14 +14,6 @@ func main() {
 
 	r = routes.IndexRouter(r)
 	r = routes.AuthRouter(r)
-
-	c := utils.GetClient()
-	err := c.Ping(context.Background(), readpref.Primary())
-	if err != nil {
-		log.Fatal("Couldn't connect to the database", err)
-	} else {
-		log.Println("Connected!")
-	}
 	fs := http.FileServer(http.Dir("./public/"))
 	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
 	http.Handle("/", r)
